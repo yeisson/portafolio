0b977ae61c10c2c6fc1307430ef1d4e9fadd3e4f
Yeisson <yeisson@gmail.com>
2018-04-29 10:31:21 -0500

    Servicio de firabase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,14 +6,28 @@ export class InformacionService {
 
   info: any = {};
   cargada = false;
+  cargada_sobre_nosotros = false;
+  equipo: any[] = [];
 
   constructor(public http: Http) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
 
+  public carga_info() {
     this.http.get('assets/data/info.pagina.json')
               .subscribe(data => {
-                console.log(data.json());
                 this.cargada = true;
                 this.info = data.json();
+              });
+  }
+
+  public carga_sobre_nosotros() {
+    this.http.get('https://mapas-1495405752278.firebaseio.com/equipo.json')
+              .subscribe(data => {
+                console.log(data.json());
+                this.cargada_sobre_nosotros = true;
+                this.equipo = data.json();
               });
   }
 
