554eaefdcba8aa62b5fc768875ea53ad4409c42d
Yeisson <yeisson@gmail.com>
2018-05-01 11:46:50 -0500

    primera version

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
+
 @Injectable()
 export class ProductosService {
 
   productos: any[] = [];
+  productos_filtrado: any[] = [];
   cargando = true;
 
   constructor( private http: Http ) {
@@ -18,16 +20,24 @@ export class ProductosService {
 
       this.cargando = false;
 
-      this.http.get('https://mapas-1495405752278.firebaseio.com/productos_idx.json')
-        .subscribe( res => {
-          // console.log(res.json());
+      let promesa = new Promise ( ( resolve, reject) => {
 
-          // setTimeout(() => {
-            this.cargando = true;
-            this.productos = res.json();
-          // }, 3000);
+          this.http.get('https://mapas-1495405752278.firebaseio.com/productos_idx.json')
+            .subscribe( res => {
+              // console.log(res.json());
 
-        });
+              // setTimeout(() => {
+                this.cargando = true;
+                this.productos = res.json();
+                resolve();
+              // }, 3000);
+
+            });
+
+      });
+
+      return promesa;
+
 
   }
 
@@ -36,6 +46,38 @@ export class ProductosService {
    */
   public cargar_producto( cod: string) {
     return this.http.get(`https://mapas-1495405752278.firebaseio.com/productos/${ cod }.json`);
+  }
+
+  /**
+   * buscar_producto
+   */
+  public buscar_producto( termino: string) {
+
+    if ( this.productos.length === 0 ) {
+      this.cargar_productos()
+        .then( () => {
+          this.filtrar_productos(termino);
+        });
+    } else {
+      this.filtrar_productos(termino);
+    }
+
+  }
+
+  private filtrar_productos(termino) {
+
+    this.productos_filtrado = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod => {
+
+      if ( prod.categoria.indexOf ( termino ) >= 0 || prod.titulo.toLowerCase().indexOf ( termino ) >= 0) {
+        this.productos_filtrado.push ( prod );
+      }
+
+      // console.log( prod );
+    });
   }
 
 }
